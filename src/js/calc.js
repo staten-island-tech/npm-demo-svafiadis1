d213dcd07cf7d3adfa596de353b248a69df612f1
svafiadis1 <57498741+svafiadis1@users.noreply.github.com>
2020-05-07 12:47:09 -0400

    parts 2 and 3

---
+++ src/js/calc.js
@@ -0,0 +1,12 @@
+export const calc = {
+    add: function(x, y){
+        return x + y;
+    },
+};
+
+/*
+export const DOMSelectors = {
+    header: document.querySelector(".header");
+}
+console.log(calc.add(2, 6));
+*/
---
+++ src/js/index.js
@@ -0,0 +1,2 @@
+import { calc } from "./calc.js";
+//console.log(calc.add(37, 86));    
